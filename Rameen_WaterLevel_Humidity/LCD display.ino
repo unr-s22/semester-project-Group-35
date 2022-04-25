cc0160dbc960e64f3cc635f901aa0a8eb86db4df
Rameen Feda <70545267+rfeda@users.noreply.github.com>
2022-04-25 07:44:40 -0700

    Add files via upload

---
+++ Rameen_WaterLevel_Humidity/LCD display.ino
@@ -0,0 +1,35 @@
+// include the library code:
+#include <LiquidCrystal.h>
+//initialise the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int resval = 0;  // holds the value
+int respin = A5; // sensor pin used
+
+void setup() {
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  
+}
+
+void loop() {
+  // Print a message to the LCD.
+  lcd.print("WATER LEVEL: ");
+  // set the cursor to column 0, line 1
+  lcd.setCursor(0, 1);
+
+  resval = analogRead(respin); //Read data from analog pin and store it to resval variable
+  if (resval <= 3) {
+    lcd.print("Empty ");
+  } else if (resval > 3 && resval <= 350) {
+    lcd.print("Low ");
+  } else if (resval > 350 && resval <= 420) {
+    lcd.print("Medium ");
+  } else if (resval >= 420) {
+    lcd.print("High ");
+  }
+  delay(1000);
+  lcd.clear();
+}
