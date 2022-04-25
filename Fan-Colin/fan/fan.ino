083854c2c0a79bf7791144c06409c2b3e11a7749
Colin <cmartires@nevada.unr.edu>
2022-04-25 12:06:24 -0700

    refactor code into functions

--- Fan-Colin/fan/fan.ino
+++ Fan-Colin/fan/fan.ino
@@ -1,15 +1,16 @@
 //Colin Martires
-//DC Motor Testing
+//DC Motor and RTC Implementation
 
 //libraries for RTC
 #include <Wire.h>
 #include "DS1307.h"
 
-// define PORT E registers
+//define PORT E registers
 volatile unsigned char* DDR_E = (unsigned char*) 0x2D;
 volatile unsigned char* PORT_E = (unsigned char*) 0x2E;
 volatile unsigned char* PIN_E = (unsigned char*) 0x2C;
 
+//define variables
 DS1307 clock;
 int input;
 
@@ -18,12 +19,7 @@ void setup()
   //set pin 3 and 5 as OUTPUT
   *DDR_E |= B00111000;
 
-  //set clock time
-  clock.begin();
-  clock.fillByYMD(2022, 4, 13);
-  clock.fillByHMS(20, 34, 0);
-  clock.fillDayOfWeek(WED);
-  clock.setTime();
+  setClockTime();
 
   Serial.begin(9600);
 }
@@ -37,19 +33,12 @@ void loop()
 
     if(input > 50)
     {
-      //set pin 5 to HIGH to ENABLE fan
-      *PORT_E = (1 << 5) | *PORT_E;
-
-      //set pin 3 to HIGH to spin fan
-      *PORT_E = (1 << 3) | *PORT_E;
-      *PORT_E = ~(1 << 4) & *PORT_E;
-      Serial.print("Fan was turned on\n");
+      setFan(1);
       printTime();
     }
     else
     {
-      *PORT_E = ~(1 << 5) & *PORT_E;
-      Serial.print("Fan was turned off\n");
+      setFan(0);
       printTime();
     }
   }
@@ -72,4 +61,35 @@ void printTime()
     Serial.print("\n");
 }
 
+//set clock time
+void setClockTime()
+{
+  clock.begin();
+  clock.fillByYMD(2022, 4, 13);
+  clock.fillByHMS(20, 34, 0);
+  clock.fillDayOfWeek(WED);
+  clock.setTime();
+}
 
+//set fan state
+//state == 1 -> fan ON
+//state == 0 -> fan OFF
+void setFan(int state)
+{
+  if(state == 1)
+  {
+    //set pin 5 to HIGH to ENABLE fan
+    *PORT_E = (1 << 5) | *PORT_E;
+
+    //set pin 3 to HIGH to spin fan
+    *PORT_E = (1 << 3) | *PORT_E;
+    *PORT_E = ~(1 << 4) & *PORT_E;
+    Serial.print("Fan was turned ON\n");
+  }
+  else if (state == 0)
+  {
+    //set pin 5 to LOW ro DISABLE fan
+    *PORT_E = ~(1 << 5) & *PORT_E;
+    Serial.print("Fan was turned OFF\n");
+  }
+}
