28e6b5116020593c20e1a5ba3404302d66b6bf8d
peytonth0mas <81597175+peytonth0mas@users.noreply.github.com>
2022-04-27 16:05:37 -0700

    Update VentAngle.ino

--- VentAngle.ino
+++ VentAngle.ino
@@ -1,16 +1,41 @@
 #include <Stepper.h>
-#define STEPS 100
-Stepper stepper(STEPS, 8, 9, 10, 11);
-int Old = 0;
+#define STEPS 64
+Stepper stebber = Stepper(STEPS, 8, 9, 10, 11);
+int OldVal = 0;
+signed int Back = -128;
+signed int Forward = 128;
+
 
 void setup() {
-  stepper.setSpeed(50);
-
+  stebber.setSpeed(200);
+  pinMode(A0, INPUT);
+  //Serial.begin(9600);
+  OldVal = analogRead(A0);
 }
 
 void loop() {
-  int New = analogRead(A0);
-  stepper.step(New - Old);
-  Old = New;
-
-}
+  int NewVal = analogRead(A0);
+  //Serial.print(New);
+  //Serial.print(" New");    for testing
+  //Serial.print("\n");
+  
+  if(NewVal > OldVal + 25){    //adding 25 allows for pot corrections
+    stebber.step(Forward);
+    //Serial.print("NEW >");
+    //Serial.print("\n");      for testing
+  }
+  else if(NewVal + 25 < OldVal){   //adding 25 allows for pot corrections
+    stebber.step(Back);
+    
+    //Serial.print("OLD > ");
+    //Serial.print("\n");
+  }
+  else{
+    
+  }
+  OldVal = NewVal;
+  //Serial.print(Old);
+  //Serial.print(" Old");
+  //Serial.print("\n");
+  
+} 
