0b6954df60eb24b62c7d7f9f4b60d8c32b617722
peytonth0mas <81597175+peytonth0mas@users.noreply.github.com>
2022-05-02 17:36:45 -0700

    Update WorkingButton.ino

--- WorkingButton.ino
+++ WorkingButton.ino
@@ -1,7 +1,7 @@
 #include <stdbool.h> 
 
-volatile bool currentStatus = false;
-volatile bool previousStatus = true;
+volatile bool currentStatus = false;   //start current status as 0 (off)
+volatile bool previousStatus = true;  //start previous status as 1 (off)
 
 volatile unsigned char* PORT_E = (unsigned char*) 0x28;
 volatile unsigned char* DDR_E = (unsigned char*) 0x27;
@@ -13,31 +13,30 @@ volatile unsigned char* mySREG = (unsigned char*) 0x3F;
 volatile unsigned char* myEICRB = (unsigned char*) 0x6A;
 
 void setup() {
-  DDRE &= ~(1 << 4);
-  PORTE |= (1 << 4);
+  DDRE &= ~(1 << 4);  //shift a 0 into bit 4 of port E (digital 2) sets 2 to input with pullup
+  PORTE |= (1 << 4);  //shift a 1 into bit digital 2
 
   DDRE |= (1 << 1);
   
-  EICRB |= (1 << ISC41);
-  EICRB &= ~(1 << ISC40);
+  EICRB |= (1 << ISC41);   //these lines make the interrupt rely on a FALLING state
+  EICRB &= ~(1 << ISC40);  //^
 
-  EIMSK |= (1 << INT4);
+  EIMSK |= (1 << INT4);   //This enables int4 (digital 2) to be an interrupt
 
-  sei();
+  sei();   //sets status register I bit to 1, according to ATMEL datasheet
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-while(!previousStatus & currentStatus){
-  PORTE |= (1 << 1);
-}
-while(previousStatus & !currentStatus){
-  PORTE &= ~(1 << 1);
-}
-
+  
+ while(!previousStatus & currentStatus){  //checks to see if previous is 0 and current is 1 (ON state) then runs included code until interrupt switches the states
+  PORTE |= (1 << 1);    //this is purely for testing with an LED
+ }
+ while(previousStatus & !currentStatus){ //checks to see if previous is 1 and current is 0 (OFF state) then runs included code until interrupt switches the states
+  PORTE &= ~(1 << 1);  //this is purely for testing with an LED
+ }
 }
 
 ISR (INT4_vect){
-  previousStatus = !previousStatus;
+  previousStatus = !previousStatus;   //the interrupt will flip the states previous/current to the opposite truth
   currentStatus = !currentStatus;
 }
